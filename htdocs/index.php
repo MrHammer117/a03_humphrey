e7c218881356ee95a2fa68af44334c27c74a50fe
Adrian Humphrey <humphar@bgsu.edu>
2021-09-27 02:32:44 -0400

    Second Commit

---
+++ htdocs/index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Assignment 03-Humphrey, Adrian</title>
+</head>
+<body>
+  <h1><a id = maintitle>Assignment 03-Humphrey, Adrian</a></h1>
+  <div>
+    <a href = "#grocery">Grocery List</a>
+    <a href = "#locations">List of Locations</a>
+    <a href = "#favoriteMovies">Favorite Movies</a>
+    <a href = "#dogBreeds">Breeds of Dogs</a>
+    <a href = "#favoriteStates">Favorite States</a>
+    <a href = "#foods">Meals</a>
+  </div>
+  <section id = "grocery">
+    <ul>
+      <li>Bread</li>
+      <li>Chicken</li>
+      <li>Water Bottles</li>
+      <li>Shredded Cheese</li>
+      <li>Eggs</li>
+      <li>Milk</li>
+    </ul>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+  <section id = "locations">
+    <ul>
+      <li>
+        New Jersey</br>
+        <ol>
+          <li>Denville</li>
+          <li>Newark</li>
+          <li>Trenton</li>
+          <li>Alantic City</li>
+        </ol>
+      </li>
+      <li>
+        Ohio</br>
+        <ol>
+          <li>Columbus</li>
+          <li>Cleveland</li>
+          <li>Cincinnati</li>
+          <li>Bowling Green</li>
+        </ol>
+      </li>
+      <li>
+        California
+        <ol>
+          <li>Los Angeles</li>
+          <li>San Diego</li>
+          <li>San Francisco</li>
+          <li>San Jose</li>
+        </ol>
+      </li>
+      <li>
+        New York</br>
+        <ol>
+          <li>New York City</li>
+          <li>Syracuse</li>
+          <li>Buffalo</li>
+          <li>Albany</li>
+        </ol>
+      </li>
+    </ul>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+  <section id = "favoriteMovies">
+    <dl>
+      <dt>Star Wars Episode VI: Return of the Jedi</dt>
+      <dd>Luke Skywalker and the Rebel alliance must take down the</br>
+         Empire, Darth Vader, The Emporer, and the new superweapons </br>
+          the Death Star II.</dd>
+
+      <dt>Star Trek: The Wrath of Khan</dt>
+      <dd>Captain Kirk and his genetically modified Nemisis Khan have a </br>
+        tense battle over the activation of the Genesis device.
+      </dd>
+
+      <dt>2001: A Space Odessey</dt>
+      <dd>Adapted from Arthur C. Clarkes story, The large Structure,</br>
+         the Monolith connects the past and future. A man and a </br>
+         sentient AI have a showdown through space and time.</dd>
+
+      <dt>The Lord of the Rings: Fellowship of the Ring</dt>
+      <dd>A hobbit named Frodo must embark on a fateful adventure</br>
+      to cast the Ruling Ring into the Fires of Mount Doom</dd>
+
+      <dt>Alien</dt>
+      <dd>The crew of a commercial spaceship is heading to a colony,</br>
+        however one of their crewmates gets attacked by an alien, who</br>
+        gives birth to a much more deadly alien. It becomes a battle for</br>
+        survival between the crew and the dangerous alien.</dd>
+    </dl>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+  <section id = "dogBreeds">
+    <table border = "1">
+      <tr>
+        <th colspan = "4">Breed stats</th>
+      </tr>
+      <tr>
+        <th>Retrievers</th>
+        <th>Bulldogs</th>
+        <th>German Shorthaired</th>
+        <th>Boxers</th>
+      </tr>
+      <tr>
+        <td>Average Height: 35cm</td>
+        <td>Average Height: 45cm</td>
+        <td>Average Height: 65cm</td>
+        <td>Average Height: 25cm</td>
+      </tr>
+      <tr>
+        <td>Average Weight: 40lbs</td>
+        <td>Average Weight: 100lbs</td>
+        <td>Average Weight: 60lbs</td>
+        <td>Average Weight: 45lbs</td>
+      </tr>
+      <tr>
+        <td>Ecample Breed: Golden Retriever</td>
+        <td>Example Breed: English Bulldog</td>
+        <td>Example Breed: Pointer German Shorthaired</td>
+        <td>Example Breed: German Boxer Dog</td>
+      </tr>
+    </table>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+  <section id = "favoriteStates">
+    <table border = "1">
+      <tr>
+        <th colspan = "6">States</th>
+      </tr>
+      <tr>
+        <th></th>
+        <th>New Jersey</th>
+        <th>California</th>
+        <th>New York</th>
+        <th>Ohio</th>
+        <th>Colorado</th>
+      </tr>
+      <tr>
+        <td>State Bird:</td>
+        <td>American Goldfinch</td>
+        <td>California quail</td>
+        <td>Eastern bluebird</td>
+        <td>Cardinal</td>
+        <td>Lark bunting</td>
+      </tr>
+      <tr>
+        <td>State Flower:</td>
+        <td>Common blue violet</td>
+        <td>California poppy</td>
+        <td>Rose</td>
+        <td>Carnation</td>
+        <td>Colorado blue columbine</td>
+      </tr>
+      <tr>
+        <td>State Tree:</td>
+        <td>Northern Red Oak</td>
+        <td>California Redwood</td>
+        <td>Sugar maple</td>
+        <td>Ohio Buckeye</td>
+        <td>Blue spruce</td>
+      </tr>
+    </table>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+  <section id = "foods">
+    <table border = "1">
+      <tr>
+        <th colspan = "5">Meals</th>
+      </tr>
+      <tr>
+        <th colspn = "2"></th>
+        <th>Breakfast</th>
+        <th>lunch</th>
+        <th>Dinner</th>
+      </tr>
+      <tr>
+        <th rowspan = "4">Foods</th>
+        <th>Bread</th>
+        <td>Roll</td>
+        <td>Sliced Bread</td>
+        <td>Garlic Bread</td>
+      </tr>
+      <tr>
+        <th>Main Course</th>
+        <td>Eggs</td>
+        <td>Turkey Sandwich</td>
+        <td>Hamburgers</td>
+      </tr>
+      <tr>
+        <th>Vegtable</th>
+        <td>Tomato</td>
+        <td>Carrots</td>
+        <td>Asparagus</td>
+      </tr>
+      <tr>
+        <th>Dessert</th>
+        <td>Toaster Strudel</td>
+        <td>Macaroons</td>
+        <td>Cake</td>
+      </tr>
+    </table>
+    <a href = "#maintitle">Back to Top</a>
+  </section>
+</body>
+</html>
